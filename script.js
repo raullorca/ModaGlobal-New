04e21ca8db4cee67188095fb169053f4a7865238
Raul Lorca <lorca.raul@gmail.com>
2025-12-10 23:07:27 +0100

    Afegida funcionalitat: validació evitar valors en blanc

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 // Funcionalitats JavaScript
 console.log('ModaGlobal: JavaScript funcionant!");
+function checkEmpty(value) {
+   if (value === '') {
+      console.error('El camp no pot esta buit');
+      return true;
+   }
+   return false;
+}
+
